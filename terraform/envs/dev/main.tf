8f12d81655f4201417d6024ec0d3e0c3090ff5d1
teofeel <teodor.perun@gmail.com>
2026-05-18 13:30:15 +0200

    [ADD] created VPC

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
   }
 }
@@ -13,10 +13,18 @@ provider "aws" {
 
 # import iam config from global/iam workspace
 data "terraform_remote_state" "iam" {
-    backend = "local"
-    config = {
-        path = "../../global/iam/terraform.tfstate"
-    }
+  backend = "local"
+  config = {
+    path = "../../global/iam/terraform.tfstate"
+  }
 }
 
 # here we are creating VPC and lambda, S3 services using created modules from them
+module "aws_vpc" {
+  source                    = "../../modules/vpc"
+  main_vpc_cidr_block       = var.main_vpc_cidr_block
+  main_vpc_instance_tenancy = var.main_vpc_instance_tenancy
+  main_subnet_cidr_block    = var.main_subnet_cidr_block
+  main_subnet_map_on_launch = var.main_subnet_map_on_launch
+  route_table_cidr_block    = var.route_table_cidr_block
+}
---
+++ terraform/envs/dev/terraform.tfvars
@@ -0,0 +1,5 @@
+main_vpc_cidr_block       = "10.0.0.0/16"
+main_vpc_instance_tenancy = "default"
+main_subnet_cidr_block    = "10.0.1.0/24"
+main_subnet_map_on_launch = true
+route_table_cidr_block    = "0.0.0.0/0"
